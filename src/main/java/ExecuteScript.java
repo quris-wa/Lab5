a0486187d29286768bb89d23e658f6f5ea9682b1
wiru <wirrru@gmai.com>
2023-03-24 22:02:33 +0300

    execute script updated

--- src/main/java/ExecuteScript.java
+++ src/main/java/ExecuteScript.java
@@ -6,22 +6,27 @@ public class ExecuteScript extends Command {
 
     @Override
     public void execute() throws Exception {
-        String argument = UserManager.argument;
-        FileReader reader = new FileReader(argument);
-        Scanner scanner = new Scanner(reader);
-        while (scanner.hasNextLine()) {
-            String commandLine = scanner.nextLine().trim();
-            String[] input = commandLine.split(" ");
-            while (input.length == 0 || input.length > 2) {
-                commandLine = scanner.nextLine().trim();
-                input = commandLine.split(" ");
+        try {
+            String argument = UserManager.argument;
+            FileReader reader = new FileReader(argument);
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNextLine()) {
+                String commandLine = scanner.nextLine().trim();
+                String[] input = commandLine.split(" ");
+                while (input.length == 0 || input.length > 2) {
+                    commandLine = scanner.nextLine().trim();
+                    input = commandLine.split(" ");
+                }
+                String command = input[0];
+                if (input.length == 2) {
+                    argument = input[1];
+                    commandList.get(command).setArgument(argument);
+                }
+                commandList.get(command).execute();
             }
-            String command = input[0];
-            if (input.length == 2) {
-                argument = input[1];
-                commandList.get(command).setArgument(argument);
-            }
-            commandList.get(command).execute();
+        }
+        catch (java.io.FileNotFoundException e){
+            System.out.println("Файл с названием " + argument + " не найден");
         }
     }
 }
